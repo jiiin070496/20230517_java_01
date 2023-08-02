6556598e8b026398cdf278d80794dea38120b1e0
jiiin070496 <jin03154@naver.com>
2023-08-02 14:42:19 +0900

    .

--- sql/Semi_project DML_DQL.sql
+++ sql/Semi_project DML_DQL.sql
@@ -2,4 +2,4 @@ desc member;
 insert into MEMBER values ('jiin0191', '0919','jin');
 
 --원본글
-insert into BOARD values (SEQ_BOARD_BNO.nextval, 'title1', 'content1', default, 'kh1', SEQ_BOARD_BNO.nextval, 0,0) ;
+insert into BOARD values (SEQ_BOARD_IDX.nextval, 'title1', 'content1', default, 'kh1', SEQ_BOARD_BNO.nextval, 0,0) ;
