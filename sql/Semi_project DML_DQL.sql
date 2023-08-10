d14e5d2a5372f03846410d33fd6c802cda0d7bf7
jiiin070496 <jin03154@naver.com>
2023-08-10 11:17:50 +0900

    mybatis

--- sql/Semi_project DML_DQL.sql
+++ sql/Semi_project DML_DQL.sql
@@ -1,14 +1,17 @@
 desc member;'
 select * from member;
+
 select * from board;
 insert into MEMBER values ('jiin0960', '0960','jin', 'a1@gmail.com');
 insert into MEMBER values ('sony0316', '0316','soni', 'a2@gmail.com');
 insert into MEMBER values ('onue0608', '0608','nue', 'a3@gmail.com');
 
-DELETE FROM board;
-DELETE FROM member;
+DELETE FROM board where mid = '쿵';
+DELETE FROM member where mid = 'srk';
 
-
+DELETE FROM member     
+    where mid = 'b'
+;
 
 --원본글
 insert into BOARD values (SEQ_BOARD_BNO.nextval, 'title1', 'content1', default, 'jiin0960', SEQ_BOARD_BNO.nextval, 0,0) ;
@@ -18,7 +21,7 @@ insert into BOARD values (SEQ_BOARD_BNO.nextval, '어렵3', 'content3', default,
 
 
 
-
 --원본글
 insert into BOARD values (SEQ_BOARD_BNO.nextval, '첫글', '이랍니다.', default, 'jiin0960', SEQ_BOARD_BNO.nextval, 0,0) ;
 
+commit;
