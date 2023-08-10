3cd0dc08ed895398af0489ae3b0f2eb892de3e38
jiiin070496 <jin03154@naver.com>
2023-08-10 17:38:34 +0900

    ..

--- sql/Semi_project DML_DQL.sql
+++ sql/Semi_project DML_DQL.sql
@@ -6,12 +6,10 @@ insert into MEMBER values ('jiin0960', '0960','jin', 'a1@gmail.com');
 insert into MEMBER values ('sony0316', '0316','soni', 'a2@gmail.com');
 insert into MEMBER values ('onue0608', '0608','nue', 'a3@gmail.com');
 
-DELETE FROM board where mid = '쿵';
+DELETE FROM board where btitle = '몽';
 DELETE FROM member where mid = 'srk';
 
-DELETE FROM member     
-    where mid = 'b'
-;
+update board set btitle = '몽', bcontent = '총이' where bno = 23;
 
 --원본글
 insert into BOARD values (SEQ_BOARD_BNO.nextval, 'title1', 'content1', default, 'jiin0960', SEQ_BOARD_BNO.nextval, 0,0) ;
