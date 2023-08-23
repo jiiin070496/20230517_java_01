ff8b9b8b8cfee84c3a8dece3add23ee59591e9ab
jiiin070496 <jin03154@naver.com>
2023-08-23 17:34:50 +0900

    Spring db연동 수정

--- 20230823_spring_db_01_hj/src/main/java/kh/lclass/db1/member/controller/MemberController.java
+++ 20230823_spring_db_01_hj/src/main/java/kh/lclass/db1/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package kh.lclass.db1.member.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import kh.lclass.db1.member.model.sevice.MemberService;
 
