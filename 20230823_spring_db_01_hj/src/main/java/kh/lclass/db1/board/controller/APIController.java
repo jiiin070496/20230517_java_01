1483460f1d35653297c152fd95dbce3a49d965e8
jiiin070496 <jin03154@naver.com>
2023-09-26 10:40:13 +0900

    API

---
+++ 20230823_spring_db_01_hj/src/main/java/kh/lclass/db1/board/controller/APIController.java
@@ -0,0 +1,42 @@
+package kh.lclass.db1.board.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class APIController {
+	private static final String serviceKey = "ey51rbqUG4nLxm%2FIMXybyk%2FsuSNe95v4aIgn4Ydn0UhcJl91gToKXy7\r\n"
+			+ "htiiqkWXyGCVxm6xyZpakkO4hk%2FLs3w%3D%3D";
+	
+	@ResponseBody
+	@RequestMapping(value="air.do", produces="application/json; charset=UTF-8")
+	public String airPollution(String location) throws IOException{
+		String url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesur eDnsty";
+		url += "?serviceKey=" + serviceKey;
+		url += "&sidoName=" + URLEncoder.encode(location, "UTF-8");
+		url += "&returnType=json";
+		
+		URL requestUrl = new URL(url);
+		HttpURLConnection urlConnection =(HttpURLConnection)requestUrl.openConnection();
+			urlConnection.setRequestMethod("GET");
+			
+		BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+		String responseText="";
+		String line;
+		while((line=br.readLine())!=null) {
+			responseText += line;
+		}
+		br.close();
+		urlConnection.disconnect();
+		return responseText;
+
+	}
+}
